5cbbc299d2339fff2d48416ebbb46fd08926e572
AndresFlorez <anflo.97@gmail.com>
2019-11-08 16:54:58 -0500

    init

---
+++ src/main/java/serviceBus/Consumer.java
@@ -0,0 +1,63 @@
+package serviceBus;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer implements Runnable, ExceptionListener {
+    public static void main(String[] args) 
+    {
+        Consumer ex = new Consumer();
+        ex.run();
+    }
+    public void run() {
+        try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616?jms.useAsyncSend=true");
+
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection("smx","smx");
+            connection.start();
+
+            connection.setExceptionListener(this);
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create the destination (Topic or Queue)
+            Destination destination = session.createQueue("salida");
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            // Wait for a message
+            Message message = consumer.receive(1000);
+
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = textMessage.getText();
+                System.out.println("Received: " + text);
+            } else {
+                System.out.println("Received: " + message);
+            }
+
+            consumer.close();
+            session.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+    }
+    public void onException(JMSException exception) {
+        // TODO Auto-generated method stub
+
+    }
+}
